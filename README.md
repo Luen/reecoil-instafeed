2677c035e5f2e89249da72c429ce885f75b04e5e
L <Luen@users.noreply.github.com>
2022-07-06 16:36:07 +1000

    update IG token to a paid service and added reecoil_adventure to the site

---
+++ README.md
@@ -0,0 +1,3 @@
+Currently using Luen's paid account to request fresh IG tokens via https://www.instant-tokens.com/
+
+If things break, you may just need to sign up for a paid account and update the token url in the code.
--- instafeed.js
+++ instafeed.js
@@ -1,60 +1,70 @@
 <style>
-#instafeed0, #instafeed1, #instafeed2 {
+.instafeed {
   height:150px;
   text-align:center;
   overflow:hidden;
 }
 
-#instafeed0 img, #instafeed1 img, #instafeed2 img {
+.instafeed img {
   height:150px; /* height:10em;*/
   width:150px; /* width:10em;*/
   object-fit:cover;
 }
 
 @media screen and (max-width: 800px) {
-  #instafeed0, #instafeed1, #instafeed2 {
+  .instafeed {
     height:250px;
   }
-  #instafeed0 img, #instafeed1 img, #instafeed2 img {
+  .instafeed img {
     height:125px;
     width:125px;
   }
 }
 
 @media screen and (max-width: 600px) {
-  #instafeed0, #instafeed1, #instafeed2 {
+  .instafeed {
     height:200px;
   }
-  #instafeed0 img, #instafeed1 img, #instafeed2 img {
+  .instafeed img {
     height:100px;
     width:100px;
   }
 }
 </style>
-<div class="instafeed">
+<div class="ig">
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <!--https://cdn.jsdelivr.net/gh/stevenschobert/instafeed.js@2.0.0/src/instafeed.min.js-->
 <script src="https://unpkg.com/instafeed.js@2.0.0/dist/instafeed.js"></script>
 <script type="text/javascript">
-  $.getJSON("https://ig.instant-tokens.com/users/c9193176-c12e-4304-adfe-41cfcee1e993/instagram/17841401991641440/token?userSecret=kemneddo5l9ndndcwovb7q", function(json) {
-    var instafeed1 = new Instafeed({
+  $.getJSON("https://ig.instant-tokens.com/users/1d55f8ff-2133-4175-9f7e-4e9b03aa6dea/instagram/17841401991641440/token?userSecret=s8agkf6gpi9wfwbrrejv6", function(json) {
+    var reecoil_dot_com = new Instafeed({
       accessToken: json.Token,
-      target: 'instafeed1',
+      target: 'reecoil_dot_com',
       limit: 15,
       template: '<a href="\{\{link\}\}"><img src="\{\{image\}\}" alt="\{\{location\}\}" loading="lazy" /></a>'
     });
-    instafeed1.run();
+    reecoil_dot_com.run();
   });
-  $.getJSON("https://ig.instant-tokens.com/users/c9193176-c12e-4304-adfe-41cfcee1e993/instagram/17841445803071624/token?userSecret=dv3s20k9o3qdb4ibj60l2", function(json) {
-    var instafeed2 = new Instafeed({
+  $.getJSON("https://ig.instant-tokens.com/users/1d55f8ff-2133-4175-9f7e-4e9b03aa6dea/instagram/17841445803071624/token?userSecret=vjy3rnfg4v81anet0a9lgq", function(json) {
+    var reecoil_arb = new Instafeed({
       accessToken: json.Token,
-      target: 'instafeed2',
+      target: 'reecoil_arb',
       limit: 15,
       template: '<a href="\{\{link\}\}"><div class="instafeed-container" style="display:inline-block;clear:both;"><img src="\{\{image\}\}" alt="\{\{location\}\}" loading="lazy" /></div></a>'
     });
-    instafeed2.run();
+    reecoil_arb.run();
+  });
+  $.getJSON("https://ig.instant-tokens.com/users/1d55f8ff-2133-4175-9f7e-4e9b03aa6dea/instagram/17841452731774870/token?userSecret=i4374zdo3rn9zx51cmjm", function(json) {
+    var reecoil_adventure = new Instafeed({
+      accessToken: json.Token,
+      target: 'reecoil_adventure',
+      limit: 15,
+      template: '<a href="\{\{link\}\}"><div class="instafeed-container" style="display:inline-block;clear:both;"><img src="\{\{image\}\}" alt="\{\{location\}\}" loading="lazy" /></div></a>'
+    });
+    reecoil_adventure.run();
   });
 </script>
-<div id="instafeed1"></div>
-<div id="instafeed2"></div>
+<div class="instafeed" id="reecoil_dot_com"></div>
+<div class="instafeed" id="reecoil_arb"></div>
+<div class="instafeed" id="reecoil_adventure"></div>
 </div>
